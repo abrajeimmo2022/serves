766f0bd00ec3313cfd335f9c4bfc9c4ca7a932c0
abrajeimmo2022 <104862725+abrajeimmo2022@users.noreply.github.com>
2023-06-22 13:17:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 1000;
 connect()
 app. listen(PORT,
-    console.log(`index run in port ${PORT}`
+    console.log(`server run in port ${PORT}`
 ));
